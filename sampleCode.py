f73e230f19c31bb7e94935f62a6479947fbf4318
hktime9 <usama24march@gmail.com>
2020-02-09 14:59:53 +0500

    added basic functionality

---
+++ sampleCode.py
@@ -0,0 +1,78 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+def setup():
+    global creds
+    global SCOPES
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+def getTasksList():
+    global service
+    results= service.tasklists().list().execute()
+    items= results.get('items', [])
+    return items
+
+def getTasks(listID):
+    global service
+    tasks= service.tasks().list(tasklist=listID).execute()
+    return tasks
+
+def printTasklist(tasks):
+    for task in tasks:
+        print(task['title'])
+
+def makeUsableList():
+    global service
+    lists= getTasksList()
+    retList= []
+    for each in lists:
+        thisTasks= getTasks(each['id'])
+        tasks= []
+        try:
+            for x in thisTasks['items']:
+                tasks.append({
+                    'title': x['title'],
+                    'id': x['id']
+                })
+        except KeyError:
+            pass
+        
+        yield {
+            'title': each['title'],
+            'id': each['id'],
+            'items': tasks
+        }
+
+def addTask(title='', notes='', due= None, listID='@default'):
+    global service
+    task= {
+        'title': title,
+        'notes': notes,
+        'due': due
+    }
+    result= service.tasks().insert(tasklist= listID, body= task).execute()
+    return result['id']
+
+SCOPES= ['https://www.googleapis.com/auth/tasks']
+creds= None
+setup()
+service= build('tasks', 'v1', credentials=creds)
+tasklist= makeUsableList()
+addTask(title="Work on SPROJ")
+for x in tasklist:
+    print(x)
